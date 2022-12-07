7da3969ea113539fb08f967cdfc2fb7701f2ec67
Thallysson <thallyssonprogramerr14anos@gmail.com>
2022-12-07 13:56:17 -0300

    importação do firebase para a flatlist

--- src/components/Destaques/FlatListDestaques.js
+++ src/components/Destaques/FlatListDestaques.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View,Image,StyleSheet } from 'react-native';
-
+import { View,Text,Image,StyleSheet } from 'react-native';
+import firebase from '../../service';
 export default function FlatListDestaques(props) {
  return (
    <View style={Estilos.ConteinerImgDestaque}>
         <Image source={{uri: props.data.img}} style={{width: 100, height: 100}}/>
+         
+        <Text></Text>
     </View>
   );
 }
