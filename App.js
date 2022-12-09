b5019882775a351e1f552a1994185f773f847243
Thallysson <thallyssonprogramerr14anos@gmail.com>
2022-12-08 23:44:01 -0300

    importado as novas telas de login e cadastro

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import TabBar from './TabBar';
+import Cadastro from './src/Telas/Tela-Cadastro/Cadastro'
+import Login from './src/Telas/Tela-Login/Login';
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +11,10 @@ export default function App(){
   return(
 
     <NavigationContainer>
-      <Stack.Navigator initialRouteName='Inicio'>
+      <Stack.Navigator initialRouteName='tab'>
+      <Stack.Screen options={{headerShown: false}} name='Login' component={Login}/> 
+         <Stack.Screen options={{headerShown: false}}name='cadaster' component={Cadastro}/> 
+
         <Stack.Screen options={{headerShown: false}} name='tab' component={TabBar}/>
       </Stack.Navigator>
     </NavigationContainer>
